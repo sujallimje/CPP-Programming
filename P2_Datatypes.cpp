23d82a6607f2a2c494d45fd2482657f8c3a395a2
SUJAL LIMJE <118969955+sujallimje@users.noreply.github.com>
2023-04-22 21:02:14 +0530

    Add files via upload

---
+++ P2_Datatypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=3;
+    float b=7.2;
+    char c='v';
+    double d=3.1416;
+    bool e = true;
+    cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl<<e;
+    return 0;
+}
